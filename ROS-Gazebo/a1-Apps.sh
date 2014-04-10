52b25ed7b4c99fa0a5e019c12cab43835b71d53a
Carlos Miguel Correia da Costa <carloscosta.cmcc@gmail.com>
2014-04-10 09:49:17 +0100

    Updated apps and added instructions on how to debug ros nodes within eclipse.

--- ROS-Gazebo/a1-Apps.sh
+++ ROS-Gazebo/a1-Apps.sh
@@ -29,25 +29,26 @@ sudo add-apt-repository ppa:kazam-team/stable-series -y # desktop recording
 sudo add-apt-repository ppa:irie/blender -y # 3d modeling
 sudo add-apt-repository ppa:shutter/ppa -y # desktop screenshots
 sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu/ precise partner' >> /etc/apt/sources.list.d/canonical_partner.list" # Skype
-# sudo add-apt-repository ppa:videolan/stable-daily -y # latest vlc
+sudo add-apt-repository ppa:videolan/stable-daily -y # latest vlc
 
 
 # >>>>> Office
-sudo apt-get purge libreoffice-core # remove old Libre Office
+sudo apt-get purge libreoffice-core -y # remove old Libre Office
 sudo add-apt-repository ppa:libreoffice/libreoffice-4-2 -y # latest Libre Office
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" # Adobe Reader
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" -y # Adobe Reader
 
 
 # >>>>> OS tools
 sudo add-apt-repository ppa:tualatrix/ppa -y # Ubuntu Tweak
 sudo add-apt-repository ppa:linrunner/tlp -y # power management
-sudo add-apt-repository ppa:atareao/atareao -y # for my-weather-indicator
+sudo add-apt-repository ppa:atareao/atareao -y # my-weather-indicator
+sudo add-apt-repository ppa:atareao/nautilus-extensions -y # pdf tools
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y # graphical ppa management
 
 
 # >>>>> Dropbox
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
-sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main" -y
 
 
 
@@ -58,7 +59,7 @@ echo "------------------------------------------------"
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade -y
 
 
 
@@ -77,7 +78,10 @@ echo "------------------------------------------------"
 # sudo update-alternatives --config gcc
 # sudo update-alternatives --config g++
 
+sudo apt-get install gdb gdb-doc gdbserver -y
 sudo apt-get install cmake cmake-gui -y
+# sudo apt-get install ccache -y
+# sudo apt-get install colorgcc -y
 sudo apt-get install oracle-java7-installer -y
 sudo apt-get install sublime-text-installer -y
 
@@ -97,8 +101,9 @@ sudo apt-get install libdvdread4 -y
 sudo /usr/share/doc/libdvdread4/install-css.sh
 sudo apt-get install my-weather-indicator -y
 sudo apt-get install vlc browser-plugin-vlc -y
+sudo apt-get install totem-plugins-extra
 sudo apt-get install kazam -y
-sudo apt-get install shutter
+sudo apt-get install shutter -y
 sudo apt-get install gimp gimp-data gimp-data-extras gimp-plugin-registry -y
 sudo apt-get install blender -y
 
@@ -112,25 +117,52 @@ sudo apt-get install libreoffice -y
 sudo apt-get install ubuntu-tweak -y
 sudo apt-get install indicator-multiload -y
 sudo apt-get install indicator-cpufreq -y
-sudo apt-get install unity-tweak-tool -y
+sudo apt-get install myunity -y # 12.04
+# sudo apt-get install unity-tweak-tool -y # 13.04
+ sudo apt-get install assogiate -y # manage file types and extensions
 sudo apt-get install tlp tlp-rdw -y
 sudo tlp start
 sudo apt-get install compiz compiz-plugins compiz-plugins-default compizconfig-settings-manager -y
 sudo apt-get install conky-all -y
-sudo apt-get install synaptic -y
 sudo apt-get install gparted -y
 sudo apt-get install filezilla -y
 sudo apt-get install y-ppa-manager -y
-sudo apt-get install --no-install-recommends gnome-panel -y
-# gnome-desktop-item-edit --create-new ~/.local/share/applications/
+sudo apt-get install --no-install-recommends gnome-panel -y # usage: gnome-desktop-item-edit --create-new ~/.local/share/applications/
 sudo apt-get install systemtap -y
-sudo apt-get install nvidia-cuda-toolkit -y
+# sudo apt-get install nvidia-cuda-toolkit -y
 
 # +++ sound equalizer
 sudo apt-get install pulseaudio-equalizer -y
 mkdir -p ~/.pulse/presets
 
+# +++ synaptics touchpad
+sudo apt-get install synaptic -y
+sudo apt-get install gsynaptics -y
+sudo apt-get install kde-config-touchpad -y
+
+
+# >>>>> nautilus extensions
+sudo apt-get install nautilus-pdf-tools -y
+sudo apt-get install nautilus-image-tools -y
+sudo apt-get install nautilus-image-converter -y
+sudo apt-get install nautilus-ideviceinfo -y
+sudo apt-get install nautilus-script-manager -y
+sudo apt-get install nautilus-script-debug -y
+sudo apt-get install nautilus-script-audio-convert -y
+sudo apt-get install nautilus-filename-repairer -y
+sudo apt-get install nautilus-actions -y --force-yes
+sudo apt-get install nautilus-columns -y --force-yes
+sudo apt-get install nautilus-compare -y --force-yes
+sudo apt-get install nautilus-open-terminal -y --force-yes
+sudo apt-get install nautilus-wallpaper -y --force-yes
+sudo apt-get install nautilus-wipe -y --force-yes
+sudo apt-get install nautilus-gtkhash -y --force-yes
+sudo apt-get install seahorse-nautilus -y
+
 
 # >>>>> Dropbox
-sudo apt-get install nautilus-dropbox -y
+sudo apt-get install dropbox -y
+
+
+# >>>>> Torrents
 sudo apt-get install deluge -y
